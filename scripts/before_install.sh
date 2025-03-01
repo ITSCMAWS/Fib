e0a84c277d7de6f5221993a225db0a3a25269393
ITSCMAWS <ITSCMAWS@uww.edu>
2025-03-01 15:29:36 -0600

    BI change

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Stop any running instance of the application
-if [ -f /home/ubuntu/fibonacci/pidfile ]; then
-    kill $(cat /home/ubuntu/fibonacci/pidfile) || true
-    rm -f /home/ubuntu/fibonacci/pidfile
-fi
+#if [ -f /home/ubuntu/fibonacci/pidfile ]; then
+#    kill $(cat /home/ubuntu/fibonacci/pidfile) || true
+#    rm -f /home/ubuntu/fibonacci/pidfile
+#fi
 
 # Ensure the deployment directory exists
 mkdir -p /home/ubuntu/fibonacci
