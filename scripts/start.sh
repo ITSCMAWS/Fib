0010cc52203fdad91101f5638a1d2282310cbfb6
ITSCMAWS <160443728+ITSCMAWS@users.noreply.github.com>
2025-03-01 13:14:40 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,5 @@ set -e
 cd /home/ubuntu/fibonacci
 
 # Start the application in the background and save the PID
-#nohup python3 fibonacci.py > /dev/null 2>&1 &
-#echo $! > /home/ubuntu/fibonacci/pidfile
+nohup python3 fibonacci.py > /dev/null 2>&1 &
+echo $! > /home/ubuntu/fibonacci/pidfile
