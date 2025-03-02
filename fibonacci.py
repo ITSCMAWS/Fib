ae3f7b857243b446dacaa9ea0d7c207223d545da
ITSCMAWS <ITSCMAWS@uww.edu>
2025-03-02 10:46:18 -0600

    yat

--- fibonacci.py
+++ fibonacci.py
@@ -12,7 +12,7 @@ def calculate_fibonacci(n):
 	return fib
 		
 def main():
-	n = 15  # Calculate first 10 Fibonacci numbers
+	n = 12  # Calculate first 10 Fibonacci numbers
 	result = calculate_fibonacci(n)
 	print(f"First {n} Fibonacci numbers: {result}")
 		
